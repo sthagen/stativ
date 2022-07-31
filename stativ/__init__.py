4310af46f769038df7d2675dc53016cc8d340cc8
Stefan Hagen <stefan@hagen.link>
2022-07-31 12:43:32 +0200

    Added some YAGNI content to the package file

---
+++ stativ/__init__.py
@@ -0,0 +1,25 @@
+"""Tripod (Denish: stativ) - manage the back end of Binaries Repository Management (BRM) systems like a tripod."""
+import os
+from typing import List
+
+APP_NAME = (
+    'Tripod (Denish: stativ) - manage the back end of Binaries Repository Management (BRM) systems like a tripod.'
+)
+APP_ALIAS = 'stativ'
+APP_ENV = 'STATIV'
+DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
+VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
+QUIET = False
+STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+DEFAULT_CONFIG_NAME = '.stativ.json'
+DEFAULT_LF_ONLY = 'YES'
+
+# [[[fill git_describe()]]]
+__version__ = '2022.7.28+parent.9fb117a4'
+# [[[end]]] (checksum: a143e8c8551090fa81324b0683ba1a40)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+__all__: List[str] = []
