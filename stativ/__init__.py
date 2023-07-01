180be3071c2c6037ce9405a3f440935e7f8e986e
Stefan Hagen <stefan@hagen.link>
2023-07-01 17:59:02 +0200

    Derived some app specific variable values from module (well file) docstring and the place

--- stativ/__init__.py
+++ stativ/__init__.py
@@ -1,19 +1,19 @@
 """Tripod (Denish: stativ) - manage the back end of Binaries Repository Management (BRM) systems like a tripod."""
 import os
+import pathlib
 from typing import List
 
-APP_NAME = (
-    'Tripod (Denish: stativ) - manage the back end of Binaries Repository Management (BRM) systems like a tripod.'
-)
-APP_ALIAS = 'stativ'
-APP_ENV = 'STATIV'
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
+APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
 DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
 VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
 QUIET = False
 STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
 ENCODING = 'utf-8'
 ENCODING_ERRORS_POLICY = 'ignore'
-DEFAULT_CONFIG_NAME = '.stativ.json'
+DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
+
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
